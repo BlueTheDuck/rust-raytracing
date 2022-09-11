d6ae33677dd1c32bf2a72c1963c4e39cbcbc6687
BlueTheDuck <hello@perezv.ar>
2022-09-11 02:38:06 -0300

    Render function now accounts for the distance of the objects, and uses the color of the closest one

--- raytracer/src/scene/mod.rs
+++ raytracer/src/scene/mod.rs
@@ -3,10 +3,12 @@ mod ray;
 
 pub use camera::Camera;
 use image::RgbImage;
-use itertools::iproduct;
 pub use ray::Ray;
 
-use crate::{color::Color, shapes::Object};
+use crate::{
+    color::Color,
+    shapes::{Intersection, Object},
+};
 
 pub fn render(
     framebuffer: &mut RgbImage,
@@ -18,10 +20,21 @@ pub fn render(
         let x = (px as f64 / width as f64) * 2.0 - 1.0;
         let y = (py as f64 / height as f64) * 2.0 - 1.0;
         let ray = camera.ray(x, y);
+        let mut closest: Option<(f64, Color)> = None;
         for obj in objects {
-            if obj.intersects(&ray) {
-                *pixel = obj.color();
+            if let Intersection::Hit(t) = obj.distance(&ray) {
+                debug_assert!(t >= 0.0 && t.is_finite());
+                if let Some((closest_t, _)) = closest {
+                    if t < closest_t {
+                        closest = Some((t, obj.color()));
+                    }
+                } else {
+                    closest = Some((t, obj.color()));
+                }
             }
+        }
+        if let Some((_, color)) = closest {
+            *pixel = color;
         }
     }
 }
